980b883d9e64ac247a936368a904dfc636b2e2e5
Edwin Cruz <ecruz165@yahoo.com>
2021-12-07 23:23:35 -0500

    REFACTOR: Added component for a widget to contain functionality of buy-me-a-coffee integration

---
+++ src/app/components/buy-me-a-coffee/buy-me-a-coffee.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buy-me-a-coffee',
+  templateUrl: './buy-me-a-coffee.component.html',
+  styleUrls: ['./buy-me-a-coffee.component.scss']
+})
+export class BuyMeACoffeeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
