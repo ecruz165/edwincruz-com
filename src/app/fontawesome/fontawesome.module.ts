1d0b4173131984b77737c9feb7c6c55bbf74da80
Edwin Cruz <ecruz165@yahoo.com>
2021-12-11 00:25:33 -0500

    FEATURE: Added additional icons used

--- src/app/fontawesome/fontawesome.module.ts
+++ src/app/fontawesome/fontawesome.module.ts
@@ -2,11 +2,11 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 import {FaIconLibrary, FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import {faBolt, faPhone} from '@fortawesome/free-solid-svg-icons';
+import {faBolt, faPhone, faDownload, faEye} from '@fortawesome/free-solid-svg-icons';
 import {faGithub, faHackerrank, faStackOverflow, faTwitter,} from '@fortawesome/free-brands-svg-icons';
 
 const fontawesomeComponents = [
-  faPhone, faBolt, faTwitter, faHackerrank, faStackOverflow, faGithub
+  faPhone, faBolt, faTwitter, faHackerrank, faStackOverflow, faGithub, faDownload, faEye
 ]
 
 @NgModule({
