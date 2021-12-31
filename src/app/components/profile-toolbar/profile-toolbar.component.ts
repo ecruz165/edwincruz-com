f72fb4793672c442deb452697a619b2f96869d9a
Edwin Cruz <ecruz165@yahoo.com>
2021-12-31 01:17:15 -0500

    FEATURE: Collapse toolbar into a drop menu for mobile view

--- src/app/components/profile-toolbar/profile-toolbar.component.ts
+++ src/app/components/profile-toolbar/profile-toolbar.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {IconProp} from "@fortawesome/fontawesome-svg-core";
+
+export interface MenuItem {
+  label: string;
+  href: string;
+  target: string;
+  iconProp: IconProp;
+  showOnMobile: boolean;
+  showOnTablet: boolean;
+  showOnDesktop: boolean;
+}
 
 @Component({
   selector: 'app-profile-toolbar',
@@ -7,7 +18,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileToolbarComponent implements OnInit {
 
-  constructor() { }
+  menuItems: MenuItem[] = [
+    {
+      label: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/edwinmcruz/',
+      target: '_external',
+      iconProp: ['fab', 'linkedin'],
+      showOnMobile: false,
+      showOnTablet: true,
+      showOnDesktop: true
+    }, {
+      label: 'Github',
+      href: 'https://github.com/ecruz165',
+      target: '_external',
+      iconProp: ['fab', 'github'],
+      showOnMobile: false,
+      showOnTablet: true,
+      showOnDesktop: true
+    }, {
+      label: 'Hackerrank',
+      href: 'https://www.hackerrank.com/ecruz165',
+      target: '_external',
+      iconProp: ['fab', 'hackerrank'],
+      showOnMobile: false,
+      showOnTablet: true,
+      showOnDesktop: true
+    }, {
+      label: 'Stackoverflow',
+      href: 'https://stackoverflow.com/users/6226761/edwin-m-cruz',
+      target: '_external',
+      iconProp: ['fab', 'stack-overflow'],
+      showOnMobile: false,
+      showOnTablet: true,
+      showOnDesktop: true
+    }, {
+      label: 'Stackblitz',
+      href: 'https://stackblitz.com/@ecruz165',
+      target: '_external',
+      iconProp: ['fas', 'bolt'],
+      showOnMobile: false,
+      showOnTablet: true,
+      showOnDesktop: true
+    }, {
+      label: 'Twitter',
+      href: 'https://twitter.com/ecruz165',
+      target: '_external',
+      iconProp: ['fab', 'twitter'],
+      showOnMobile: false,
+      showOnTablet: true,
+      showOnDesktop: true
+    }
+  ]
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
