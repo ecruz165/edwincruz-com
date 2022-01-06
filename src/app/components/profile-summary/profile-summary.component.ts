0f7d84e95b656b4777107f603083b36de5b85eef
Edwin Cruz <ecruz165@yahoo.com>
2022-01-06 01:06:05 -0500

    FEATURE: User shall be able to see overview when user's mouse hovers above profile title and link to see resume.

--- src/app/components/profile-summary/profile-summary.component.ts
+++ src/app/components/profile-summary/profile-summary.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import {animate, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-profile-summary',
+  animations: [
+  trigger('myInsertRemoveTrigger', [
+    transition(':enter', [
+      style({ opacity: 0 }),
+      animate('10s', style({ opacity: 1 })),
+    ]),
+    transition(':leave', [animate('10s', style({ opacity: 0 }))]),
+  ]),
+],
   templateUrl: './profile-summary.component.html',
   styleUrls: ['./profile-summary.component.scss']
 })
 export class ProfileSummaryComponent implements OnInit {
 
+  viewable = false
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  toggle() {
+    this.viewable = !this.viewable;
+  }
 }
