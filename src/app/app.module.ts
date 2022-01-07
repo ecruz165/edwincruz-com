468390dd1b53ed91d7fe0303516e76a9757526b9
Edwin Cruz <ecruz165@yahoo.com>
2022-01-07 01:41:12 -0500

    FEATURE: Added a footer with a copyright.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import {AppThemeService} from "./app-theme.service";
 import {TransferStateInterceptor} from "./interceptors/transfer-state.interceptor";
 import {HTTP_INTERCEPTORS} from "@angular/common/http";
 import {AppSplashScreenMatrixService} from "./app-splash-screen-matrix.service";
+import {FooterComponent} from "./components/footer/footer.component";
 
 export function showSplashScreen(splashScreenService: AppSplashScreenMatrixService) {
   return (): Promise<any> => {
@@ -40,6 +41,7 @@ export function initializeAppTheme(appThemeService: AppThemeService) {
 @NgModule({
   declarations: [
     AppComponent,
+    FooterComponent,
     ProfileToolbarComponent,
     ProfileCardComponent,
     WorkHistoryCarouselComponent,
