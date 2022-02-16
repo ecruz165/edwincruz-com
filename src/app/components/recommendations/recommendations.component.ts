9ea5556d6efaa9ff4170392dea10e255d8f9ad6d
Edwin Cruz <ecruz165@yahoo.com>
2022-02-16 01:37:05 -0500

    CLEANUP: Optimized image size for Recommended industry experts.

--- src/app/components/recommendations/recommendations.component.ts
+++ src/app/components/recommendations/recommendations.component.ts
@@ -21,43 +21,43 @@ export class RecommendationsComponent implements OnInit {
       expertise: ['Advanced Java', 'Patterns'],
       website: 'https://www.javaspecialists.eu/',
       target: '_external',
-      picUrl: 'https://www.filepicker.io/api/file/WA9EOsGoRhG687gr1TlS'
+      picUrl: '/assets/pics/dr-heinz-m-kabutz-200.jpg'
     }, {
       name: 'Eugen Baeldung',
-      expertise: ['Spring REST', 'Spring Security', 'Spring Data'],
+      expertise: ['Spring REST', 'Spring Data', 'Spring Security'],
       website: 'https://www.baeldung.com/',
       target: '_external',
-      picUrl: 'https://www.baeldung.com/wp-content/uploads/2018/02/photo-eugen.png'
+      picUrl: '/assets/pics/eugen-baeldung-200.jpg'
     }, {
       name: 'Vlad Mihalcea',
       expertise: ['Java', 'Hibernate', 'JPA', 'JDBC'],
       website: 'https://vladmihalcea.com/',
       target: '_external',
-      picUrl: 'https://avatars.githubusercontent.com/u/5571399?v=4'
+      picUrl: '/assets/pics/vlad-mihalcea-200.jpg'
     }, {
       name: 'Philip Rieckpil',
       expertise: ['Spring', 'Java', 'Testing'],
       website: 'https://rieckpil.de/',
       target: '_external',
-      picUrl: 'https://cdn-aoloc.nitrocdn.com/LjZAnpMBiKRdWkhommfnyugsAOnHWdxL/assets/static/optimized/rev-ec83180/wp-content/uploads/2020/07/rieckpilParkBench-585x703.jpg.webp'
+      picUrl: '/assets/pics/philip-rieckpil-200.jpg'
     }, {
       name: 'Dmytro Mezhenskyi',
       expertise: ['Angular', 'RXJS'],
       website: 'https://www.decodedfrontend.io/',
       target: '_external',
-      picUrl: 'https://pbs.twimg.com/profile_images/1428302019777998855/FePGJNv5_400x400.jpg'
+      picUrl: '/assets/pics/dmytro-mezhenskyi-200.jpg'
     }, {
-      name: 'Basarat Ali Syed',
+      name: 'Basarat Ali Syed ',
       expertise: ['Typescript'],
       website: 'https://basarat.com/',
       target: '_external',
-      picUrl: 'https://avatars.githubusercontent.com/u/874898?v=4'
+      picUrl: '/assets/pics/basarat-ali-syed-200.jpg'
     }, {
       name: 'Borislav Hadzhiev',
       expertise: ['AWS', 'CDK', 'Javascript',],
       website: 'https://bobbyhadz.com/',
       target: '_external',
-      picUrl: 'https://bobbyhadz.com/images/global/avatar-big.webp'
+      picUrl: '/assets/pics/borislav-hadzhiev-200.jpg'
     }
   ]
 
