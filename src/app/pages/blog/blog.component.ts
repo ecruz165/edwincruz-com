c670fc2479a2852288ad31da876b2927d9d24e19
Edwin Cruz <ecruz165@yahoo.com>
2022-02-27 01:11:39 -0500

    FEATURE: Rearrange implementation to support SSR

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -1,8 +1,14 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import {DOCUMENT} from "@angular/common";
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {BlogService} from "../../services/blog.service";
 import {Blog} from "../../services/blog.model";
+import {MarkdownConverterService} from "../../services/markdown-converter.service";
+import {filter, Observable, of, switchMap} from "rxjs";
+import {take, tap} from "rxjs/operators";
+
+function isDefined<T>(arg: T | null | undefined): arg is T {
+  return arg !== null && arg !== undefined;
+}
 
 @Component({
   selector: 'app-blog',
@@ -12,24 +18,44 @@ import {Blog} from "../../services/blog.model";
 export class BlogComponent implements OnInit {
   url: string = '';
   blog?: Blog;
+  parsedMarkdown: string = '';
 
   constructor(
-    @Inject(DOCUMENT) public document: Document,
     private route: ActivatedRoute,
-    private blogService: BlogService) {
+    private blogService: BlogService,
+    private markdownConverterService: MarkdownConverterService) {
   }
 
   ngOnInit(): void {
+      this.getBlogKeyFromPath()
+        .pipe(
+          switchMap(key => this.findBlogByKey(key)),
+          switchMap( blog => this.loadBlogAsConvertedMarkdown(blog.postPath+blog.postFileName))
+        ).subscribe( next =>{
+          this.parsedMarkdown = next;
+      })
+  }
+
+  private getBlogKeyFromPath(): Observable<string> {
     const id = this.route.snapshot.paramMap.get('id');
-    if (id !== undefined && id !== null) {
-      this.blogService.getBlogPostByKey(id)
-        .subscribe(next => {
-          this.blog = next;
-          if (next != undefined){
-            this.url = next.postPath + next.postFileName;
-          }
-        })
+    if (id !== undefined && id !== null && id.length ! > 10) {
+      return of(id);
+    } else {
+      throw Error('invalid blog key');
     }
+  }
+
+  private findBlogByKey(key: string): Observable<Blog> {
+    return this.blogService.getBlogPostByKey(key)
+      .pipe(
+        filter(isDefined),
+        take(1),
+        tap(blog=> this.blog = blog)
+      )
+  }
+
+  private loadBlogAsConvertedMarkdown(url: string): Observable<string> {
+    return this.markdownConverterService.convert(url);
   }
 
 }
--- src/app/pages/blog/blog.module.ts
+++ src/app/pages/blog/blog.module.ts
@@ -1,12 +1,10 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {BlogComponent} from "./blog.component";
-
 import {RouterModule, Routes} from '@angular/router';
 import {BlogLayoutComponent} from "./blog-layout.component";
 import {AppLayoutModule} from "../../layout/layout.module";
 import {FlexLayoutModule} from "@angular/flex-layout";
-import {MarkdownViewerModule} from "../../modules/markdown-viewer/markdown-viewer.module";
 import {BlogListComponent} from './blog-list/blog-list.component';
 import {MaterialModule} from "../../modules/material/material.module";
 import {FontawesomeSetModule} from "../../modules/fontawesome/fontawesome.module";
@@ -33,7 +31,6 @@ const routes: Routes = [
     AppLayoutModule,
     FlexLayoutModule,
     RouterModule.forChild(routes),
-    MarkdownViewerModule,
     FontawesomeSetModule,
     MaterialModule,
 
